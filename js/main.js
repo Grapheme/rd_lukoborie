2f2a47c6e58ac84249e5cd59d2bc28a4e8fbb45d
KostyaD <thedamaxstudio@gmail.com>
2013-12-03 14:31:23 +0400

    fade.js

--- js/main.js
+++ js/main.js
@@ -412,28 +412,4 @@ function fetchGalleryItems() {
 
 		return defer.promise();
 	};
-// ----------------------------------------------------------------------------
-// Функция анимации элементов
-// ----------------------------------------------------------------------------	
-	function timeOutFade($elems, $fadeElems, timer, step) {
-		if($elems[0]) {
-			$fadeElems.css({ 'opacity': '1' });
-			$elems.each( function(){
-				var that = $(this);
-				setTimeout( function(){
-					that.css({ 'opacity': '1', '-moz-transform': 'translate(0px, 0px)', '-o-transform': 'translate(0px, 0px)', '-webkit-transform': 'translate3d(0px, 0px, 0px)', '-ms-transform': 'translate(0px, 0px)', 'transform': 'translate(0px, 0px)'}); 
-				}, timer);	
-				timer += step;
-			});			
-		}
-	}
-	function animateOnLoad() {			
-		/*Анимация происходит при помощи transition*/		
-		var $leftElems = $('.left_dir');
-		var $rightelems = $('.right_dir');
-		var $fadeElems = $('.w_fade');						
-		//$elems.css({ '-moz-transform': 'translate(0px, 0px)', '-o-transform': 'translate(0px, 0px)', '-webkit-transform': 'translate3d(0px, 0px, 0px)', '-ms-transform': 'translate(0px, 0px)', 'transform': 'translate(0px, 0px)'}); 
-		timeOutFade($leftElems, $fadeElems, 0, 400);
-		timeOutFade($rightelems, $fadeElems, 0, 300);
-	}
 });
