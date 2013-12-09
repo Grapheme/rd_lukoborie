07e7b4093887870ccd840c367fc9da3637990a8b
KostyaD <thedamaxstudio@gmail.com>
2013-12-09 14:27:30 +0400

    anim

--- js/expert.js
+++ js/expert.js
@@ -33,6 +33,61 @@ $('.slider-to-left').click(function() {
 	return false;
 });
 
+$(document).scroll(function(){
+	fadeit('.exp-anim-1');
+	fadeit('.exp-anim-2');
+	fadeitOf('.exp-anim-3', '.exp-anim-for-3');
+	fadeit('.exp-anim-4');
+	if(isScrolled('.exp-anim-4')) {
+		setTimeout(function(){justFade('.exp-anim-5');},500);
+		setTimeout(function(){justFade('.exp-anim-6');},1000);
+		setTimeout(function(){justFade('.exp-anim-7');},2000);
+	}
+	fadeit('.exp-anim-8');
+	if(isScrolled('.exp-anim-8')) {
+		setTimeout(function(){justFade('.exp-anim-9');},500);
+		setTimeout(function(){justFade('.exp-anim-10');},1000);
+	}
+});
+
+$(function(){
+});
+
+var animload = false;
+
+function fadeit(el) {
+	if(isScrolled(el)) {
+		$(el).css('opacity', 1);
+	}
+}
+
+function fadeItAfter(el, time) {
+	setTimeout(function(){ fadeit(el); }, time);
+}
+
+function fadeitOf(el, of) {
+	if(isScrolled(of)) {
+		$(el).css('opacity', 1);
+	}
+}
+
+function justFade(el) {
+	$(el).css('opacity', 1);
+}
+
 function getCurrentSlideId() {
 	$('.current-slide').html(fotorama.activeIndex+1);
+}
+
+function isScrolled(elem)
+{
+    var $topp = $(elem).position().top;
+    var $bottomp = $topp + $(elem).height();
+    var $windowh = $(window).height();
+    var $amountw = $windowh*0.2;
+    if($(document).scrollTop() > $topp-$windowh+$amountw && $(document).scrollTop() < $bottomp ) {
+    	return true;
+    } else {
+    	return false;
+    }
 }
