c13852e2e4bd3092a248d0fab8a71bdc76716f44
Andrey S <andrey@grapheme.ru>
2013-12-12 12:22:42 +0400

    utm metki

--- js/forms.js
+++ js/forms.js
@@ -10,14 +10,14 @@ $('.sect-form .line-submit').click( function() {
 	});
 	$(document).on('click', '.camera .line-submit', function(e) {
 		var prevHeight = $(this).parent().parent().height();
-		$(this).parent().parent().find('.dc-form-capt').html('Проверьте e-mail и покупай Lumix GX7 со скидкой <a href="http://panasoniceplaza.ru/products/digital_av/digital_still_camera/lumix/DMC-GX7K/" class="buy-btn">Купить</a>');
+		$(this).parent().parent().find('.dc-form-capt').html('Проверьте e-mail и покупай Lumix GX7 со скидкой <a href="http://panasoniceplaza.ru/products/digital_av/digital_still_camera/lumix/DMC-GX7K/?utm_source=lookatme&utm_medium=promosite&utm_campaign=lumixgx7" class="buy-btn">Купить</a>');
 		$(this).parent().hide();
 		$(this).parent().parent().height(prevHeight);
 	});
 	$(document).on('submit', '.discount-form', function(e) {
 		e.preventDefault();
 		var prevHeight = $(this).parent().height();
-		$(this).parent().find('.dc-form-capt').html('Проверьте e-mail и покупай Lumix GX7 со скидкой <a href="http://panasoniceplaza.ru/products/digital_av/digital_still_camera/lumix/DMC-GX7K/" class="buy-btn">Купить</a>');
+		$(this).parent().find('.dc-form-capt').html('Проверьте e-mail и покупай Lumix GX7 со скидкой <a href="http://panasoniceplaza.ru/products/digital_av/digital_still_camera/lumix/DMC-GX7K/?utm_source=lookatme&utm_medium=promosite&utm_campaign=lumixgx7" class="buy-btn">Купить</a>');
 		$(this).hide();
 		$(this).parent().height(prevHeight);
 	});
